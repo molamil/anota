db933492c9e29b3c9c8622cd7688896d44473d42
Jorge Hernandez <jh@molamil.com>
2017-05-10 20:46:16 +0200

    Start playing with SVG.js

--- src/main.js
+++ src/main.js
@@ -1,12 +1,21 @@
-const anota = {
-    hola() {
-        return (message) => {
-            /* eslint-disable no-undef */
-            alert(`Hola ${message}?`)
-        }
-    },
-}
+document.addEventListener('DOMContentLoaded', () => {
+    console.log('DOM fully loaded and parsed')
 
-anota.hola()('PIZZA!')
+    let n = 1
+
+    const wrapper = document.createElement('div')
+    wrapper.id = `_anota${n}`
+    wrapper.setAttribute('style', 'z-index: 100000; ' +
+        'position: absolute; ' +
+        'top: 0; ' +
+        'left: 0; ' +
+        'width: 100%; ' +
+        'height: 100%; ')
+    n += 1
+    document.body.appendChild(wrapper)
+
+    const draw = SVG(wrapper.id)
+    draw.rect(100, 100)
+})
 
 // export default { anota }
