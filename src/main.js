e2e812aa1b8a4361910ce60a73bcbd0a2ddf61fb
Jorge Hernandez <jh@molamil.com>
2017-05-11 12:37:08 +0200

    Point and drag to create arrow

--- src/main.js
+++ src/main.js
@@ -4,14 +4,6 @@ const conf = {
 
 // -- ARROW CLASS
 
-// Arrow structure
-const arrowConf = {
-    t: 15,  // Shaft thickness
-    tl: 8,  // Head back tip in x
-    tt: 20, // Head back tip in y
-    tp: 50, // Head length
-}
-
 class Arrow {
     constructor(svg, x = 0, y = 0) {
         console.log('Arrow')
@@ -26,28 +18,38 @@ class Arrow {
         this.enabled = false
         this.shape = null
 
-        // Binders for "this" to work in listeners
+        // Binders so "this" to work in listeners
         this._onMove = (event) => {
             that._moveListener.call(that, event)
+        }
+        this._onDown = (event) => {
+            that._downListener.call(that, event)
+        }
+        this._onUp = (event) => {
+            that._upListener.call(that, event)
         }
     }
 
     enable() {
         this.enabled = true
-        this.wrapper.addEventListener('mousedown', this._downListener)
-        this.wrapper.addEventListener('mouseup', this._upListener)
+        this.wrapper.addEventListener('mousedown', this._onDown)
+        this.wrapper.addEventListener('mouseup', this._onUp)
     }
 
     disable() {
         this.enabled = false
-        this.wrapper.removeEventListener('mousedown', this._downListener)
-        this.wrapper.removeEventListener('mouseup', this._upListener)
-        this.wrapper.removeEventListener('mousemove', this._onMove)
+        this.stopDraw()
+        this.wrapper.removeEventListener('mousedown', this._onDown)
+        this.wrapper.removeEventListener('mouseup', this._onUp)
     }
 
-    draw() {
+    startDraw() {
         this.shape = this.svg.polygon().fill(conf.color)
         this.wrapper.addEventListener('mousemove', this._onMove)
+    }
+
+    stopDraw() {
+        this.wrapper.removeEventListener('mousemove', this._onMove)
     }
 
     _doDraw() {
@@ -61,26 +63,23 @@ class Arrow {
         this.shape.plot([
             [x1, y1],
             [x1 + al, y1],
-            [x1 + al + (-arrowConf.tl), y1 - arrowConf.tt],
-            [x1 + al + arrowConf.tp, y1 + (arrowConf.t / 2)],
-            [x1 + al + (-arrowConf.tl), y1 + arrowConf.t + arrowConf.tt],
-            [x1 + al, y1 + arrowConf.t],
-            [x1, y1 + arrowConf.t],
+            [x1 + al + (-Arrow.tl), y1 - Arrow.tt],
+            [x1 + al + Arrow.tp, y1 + (Arrow.t / 2)],
+            [x1 + al + (-Arrow.tl), y1 + Arrow.t + Arrow.tt],
+            [x1 + al, y1 + Arrow.t],
+            [x1, y1 + Arrow.t],
         ])
-        console.log('a', a)
-        this.shape.rotate(a, x1, y1 + (arrowConf.t / 2))
+        this.shape.rotate(a, x1, y1 + (Arrow.t / 2))
     }
 
     _downListener(event) {
         this.x1 = event.clientX
         this.y1 = event.clientY
-        this.draw()
+        this.startDraw()
     }
 
-    _upListener(event) {
-        this.x1 = event.clientX
-        this.y1 = event.clientY
-        this.draw()
+    _upListener() {
+        this.stopDraw()
     }
 
     _moveListener(event) {
@@ -90,40 +89,51 @@ class Arrow {
     }
 }
 
+// Static properties
+Arrow.t = 15  // Shaft thickness
+Arrow.tl = 8  // Head back tip in x
+Arrow.tt = 20 // Head back tip in y
+Arrow.tp = 50 // Head length
+
 
 // -- ANOTA CLASS
 
-const anotaConf = {
-    n: 1,
-}
-
 class Anota {
-    constructor(id = `_anota${anotaConf.n}`) {
+    constructor(id = `_anota${Anota.n}`) {
         console.log('Anota')
 
         const wrapper = document.createElement('div')
         wrapper.id = id
         wrapper.setAttribute('style', 'z-index: 100000; ' +
             'position: absolute; ' +
+            'overflow: hidden; ' +
             'top: 0; ' +
             'left: 0; ' +
             'width: 100%; ' +
             'height: 100%; ')
         // TODO: Consider checking whether document.body is not undefined and wait for DOM loaded
         document.body.appendChild(wrapper)
-        anotaConf.n += 1
+        Anota.n += 1
 
         this.wrapper = wrapper
         this.svg = SVG(id)
+        this.currentTool = null
         this.arrows = []
+    }
+
+    selectArrow() {
+        console.log(this)
     }
 
     drawArrow() {
         const arrow = new Arrow(this.svg)
-        arrow.draw()
+        arrow.enable()
         this.arrows.push(arrow)
     }
 }
+
+Anota.n = 1
+
 
 // TODO: Try to achieve this directly on the UMD wrapper, right now it outputs on window.main.Anota
 window.Anota = Anota
